887be8a4c9a6b6e72de5993361baca745114e754
Rakesh Kumar Mandal <rakesh_mandal23@manavrachna.net>
2026-06-14 14:12:17 +0530

    fix resume download url

--- client/src/components/Resume.jsx
+++ client/src/components/Resume.jsx
@@ -1,9 +1,9 @@
 export default function Resume() {
   const handleDownload = () => {
-  window.open(
-    `${import.meta.env.VITE_API_URL}/resume/download`,
-    '_blank'
-  );
+    window.open(
+      'https://rakeshkumarmandalportfolio.onrender.com/api/resume/download',
+      '_blank'
+    );
   };
 
   return (
