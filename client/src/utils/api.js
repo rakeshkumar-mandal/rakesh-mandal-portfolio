9b66003004297a99b3a442eb5b5f5efe8e6f9546
Rakesh Kumar Mandal <rakesh_mandal23@manavrachna.net>
2026-06-14 12:45:00 +0530

    Connect frontend to render backend

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://rakeshkumarmandalportfolio.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
   timeout: 10000,
 });
